c4b419f9a642ced7e961676cbc969e78230b7d72
Keyur Vadodariya <vadodariyakeyur321@gmail.com>
2020-12-28 17:23:49 +0530

    First commit, Added REST API to access Items, Stores and users

---
+++ resources/store.py
@@ -0,0 +1,53 @@
+from flask_restful import Resource, reqparse
+from flask_jwt import jwt_required
+
+from models.store import StoreModel
+
+class Store(Resource):
+
+    parser = reqparse.RequestParser()
+    parser.add_argument('name',
+        type=str,
+        required=True,
+        help="This field cannot be left blank"
+    )
+
+    @jwt_required()
+    def get(self,name):
+        store = StoreModel.find_by_name(name)
+        if store:
+            return store.json()
+        
+        return {"message" : "Store not Found"} , 404
+
+    @jwt_required()
+    def post(self, name):
+        if StoreModel.find_by_name(name):
+            return {'message': "A Store with name '{}' already exists.".format(name)}
+
+        store = StoreModel(name)
+
+        try:
+            store.save_to_db()
+        except:
+            return {"message": "An error occurred inserting the item."}, 500
+
+        return store.json()
+
+    @jwt_required()
+    def delete(self,name):
+
+        store = StoreModel.find_by_name(name)
+
+        if store:
+            store.delete_from_db()
+            return {"message" : f"Item {name} Deleted"}, 200            
+
+        return {"message" : f"Item {name} Not Found"}, 404
+
+
+class StoreList(Resource):
+
+    @jwt_required()
+    def get(self):
+        return {"stores" : [store.json() for store in StoreModel.query.all()]}, 200
